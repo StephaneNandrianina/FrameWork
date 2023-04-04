ee5675c09662f3965196a4cdb028dd020616cb05
Stephane <stephaneramiandrisoa64@gmail.com>
2023-04-04 04:47:36 +0300

    sprint3
    trois

---
+++ WEB-INF/classes/Emp.java
@@ -0,0 +1,21 @@
+package etu1851.model;
+import etu1851.annotation.*;
+public class Emp {
+    int id ;
+    String nom;
+    @ClassAnnotation(id = "emp/all")
+    public String findAll(){
+        return "ok";
+    }
+
+    @ClassAnnotation(id = "/affiche")
+    public String findAll1(){
+        return "ok";
+
+    }
+    @ClassAnnotation(id = "select")
+    public String select(){
+        return "selection";
+
+    }
+}
